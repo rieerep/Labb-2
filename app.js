127dbd4ef67264e5ec040a73e31d4107df23d0e7
rieerep <114030611+rieerep@users.noreply.github.com>
2022-11-12 17:56:04 +0100

    Finalizing webpage
    squooshed imgs, changed style, added text on modals

--- app.js
+++ app.js
@@ -1,58 +1,96 @@
+window.onload = function () {
 
-// --------------------- JSON -------------------------- //
-let url = "/data.json"
-console.log("url: ", url);
-const workList = document.querySelector(".workList")
-console.log('workList', workList);
-const educationList = document.querySelector(".educationList")
 
-async function getToDo() {
+    // --------------------- JSON -------------------------- //
+    let url = "/data.json"
+    let workList = document.querySelector(".workList")
+    console.log('workList', workList);
+    const educationList = document.querySelector(".educationList")
 
-    let response = await fetch(url);
-    console.log('response:', response);
-    if (response.ok) { // if HTTP-status is 200-299
-        // get the response body (the method explained below)
-        let data = await response.json();
-        console.log('data', data)
+    async function getToDo() {
 
-        // WORK
-        for (let i = 0; i < data.work.length; i++) {
-            let li = document.createElement("li");
+        let response = await fetch(url);
+        console.log('response:', response);
+        if (response.ok) { // if HTTP-status is 200-299
+            // get the response body (the method explained below)
+            let data = await response.json();
+            console.log('data', data)
 
-            li.innerHTML = `<h4>${data.work[i].time}</h4>
+            // WORK
+            for (let i = 0; i < data.work.length; i++) {
+                let li = document.createElement("li");
+
+                li.innerHTML = `<h4>${data.work[i].time}</h4>
             <p>
-            <span>${data.work[i].title}</span><br>
-            <span>${data.work[i].desc}</span><br>
+            <em>${data.work[i].title}</em><br>
+            ${data.work[i].desc}<br>
             </p>`;
+                workList.appendChild(li);
 
-            console.log('li: ', li)
-            workList.appendChild(li);
+            }
 
-        }
+            // EDUCATION
+            for (let i = 0; i < data.education.length; i++) {
+                let li = document.createElement("li");
 
-        // EDUCATION
-        for (let i = 0; i < data.education.length; i++) {
-            console.log('i', i)
-            let li = document.createElement("li");
-
-            li.innerHTML = `<h4>${data.education[i].time}</h4>
+                li.innerHTML = `<h4>${data.education[i].time}</h4>
             <p>
-            <span>${data.education[i].title}</span>
-            <span>${data.education[i].desc}</span><br>
+            <em>${data.education[i].title}</em>
+            ${data.education[i].desc}<br>
             </p>`;
+                educationList.appendChild(li);
 
-            console.log('li: ', li)
-            educationList.appendChild(li);
+            }
 
+
+            // gives us the json of the response body
+
+
+        } else {
+            console.log("HTTP-Error: " + response.status);
         }
-
-
-        // gives us the json of the response body
-
-
-    } else {
-        console.log("HTTP-Error: " + response.status);
     }
-}
 
-getToDo();
+    getToDo();
+
+    // ---------------- MODAL BUTTON 1 ----------------- //
+    let firstBtn = document.querySelector(".modal-btn1");
+    const modal = document.querySelector(".modal1")
+    const closeBtn1 = document.querySelector(".close-btn1")
+
+    // Öppnar projekt 1:s fönster
+    firstBtn.addEventListener('click', ReadMore1);
+
+    function ReadMore1() {
+        modal.style.display = "block"
+    }
+
+    //stänger projekt 1:s fönster
+    closeBtn1.addEventListener('click', closeButtonOne);
+
+    function closeButtonOne() {
+        modal.style.display = "none"
+    }
+    // ---------- MODAL BUTTON 2 ------------------ //
+
+    const secondBtn = document.querySelector(".modal-btn2");
+    const modal2 = document.querySelector(".modal2")
+    const closeBtn2 = document.querySelector(".close-btn2")
+
+    // Öppnar projekt 2:s fönster
+    secondBtn.addEventListener('click', ReadMore2);
+
+    function ReadMore2() {
+        console.log('test');
+        modal2.style.display = "block"
+    }
+
+    //stänger projekt 2:s fönster
+    closeBtn2.addEventListener('click', closeButtonTwo);
+
+    function closeButtonTwo() {
+        console.log('close button two', closeBtn2);
+        modal2.style.display = "none"
+    }
+    let date = document.getElementById("date");
+}
